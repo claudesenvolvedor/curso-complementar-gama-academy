9537e6f37571ee496f0e57f0de8d50741d9d21e6
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-03-17 08:08:25 -0300

    adicionado o terceiro Exercício

---
+++ Aula01/src/SequenciaNumerica.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SequenciaNumerica {
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		int numero;
+		int contador = 0;
+
+		System.out.println("Digite um número");
+		numero = teclado.nextInt();
+		contador = 0;
+		System.out.println("A sequência crescente é : ");
+
+		while (contador <= numero) {
+			System.out.println(contador);
+			contador++;
+		}
+	}
+}

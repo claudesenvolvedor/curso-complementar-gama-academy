55059e139337ca513da4c05520a64b69a11213a6
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-03-22 17:06:47 -0300

    adicionado os novos teste unitários da ula 06 testando Calculadora

---
+++ aula06novostestes/ProjetoTests/src/tests/CalculadoraTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.Calculadora;
+
+public class CalculadoraTest {
+private Calculadora c;
+
+@BeforeEach
+
+
+public void setup() {
+	c = new Calculadora();
+	
+}
+@Test
+
+public void deveRetornarAsomaDosNumero() {
+	double resultado = c.somar(10, 20);
+	assertEquals(30, resultado);
+	
+}
+@Test
+public void deveRetornarAsubtracaoDosNumeros() {
+	double resultado = c.subtrair(20, 10);
+	assertEquals(10, resultado);
+	
+}
+@Test
+public void deveRetornarAmultiplicacaoDosNumeros() {
+	double resultado = c.multiplicar(2, 3);
+	assertEquals(6, resultado);
+	
+}
+@Test
+public void deveRetornarAdivisaoQuandoQuandoAdivisaoDiferenteDeZero() {
+	double resultado = c.subtrair(2, 2);
+	assertEquals(1, resultado);
+	
+}
+@Test
+public void deveRetornarValorNegativoQuandoNumeroDivisorIgualAzero() {
+	double resultado = c.dividir(1 , 0);
+	assertEquals(-1, resultado);
+	
+}
+}

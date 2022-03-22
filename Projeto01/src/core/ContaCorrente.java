20de59e58abe4c90fbd93e9544a13bf53b9a518e
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-03-22 15:43:27 -0300

    adicionada a declaração de class Abstract na class Conta

--- Projeto01/src/core/ContaCorrente.java
+++ Projeto01/src/core/ContaCorrente.java
@@ -1,4 +1,26 @@
-package core;
+DDDpackage core;
+
+public class ContaCorrente extends Conta {
+	public boolean saca(double valor) {
+		if (this.getSaldo(0) >= valor) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}DDpackage core;
+
+public class ContaCorrente extends Conta {
+	public boolean saca(double valor) {
+		if (this.getSaldo(0) >= valor) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}DDDpackage core;
 
 public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {

ab41948231b8204c4010cc2e5da27653e1c530d2
Claudeci Pinheiro <claudesenvolvedor@gmail.com>
2022-03-24 16:52:12 -0300

    correção da class GerenciaContas na criação do ArrayList

--- Projeto01/src/util/GerenciaContas.java
+++ Projeto01/src/util/GerenciaContas.java
@@ -5,47 +5,53 @@ import java.util.ArrayList;
 import core.Conta;
 
 public class GerenciaContas {
-
 	private ArrayList<Conta> contas;
+	
 	public GerenciaContas() {
 		contas = new ArrayList<>();
 		
 	}
-	
-	//fim do ArrayList
-	
-	public void adicionarConta(Conta c) {
+	public void adicionar(Conta c) {
 		contas.add(c);
 		
 	}
-	/*
-	public String exibir(int numero) {
-		for (int i = 0; i < contas.size(); i++) {
-			if(contas.get(i).getNumero() == numero) {
-				return contas.get(i).toString();
-			}
+
+	public String busca(int numero) {
+	for(int i = 0; i < contas.size(); i++ ) {
+		if(contas.get(i).getNumero(0) == numero) {
+			return contas.get(i).toString();
 		}
-		return "não encontrado";
 	}
-	*/
+	return "A Conta Não foi encontrada";
+	}
+	//método privado para fazer uma busca na Array list por  posição por pessoa
 	
-	private  int posicao(int numero) {
+	private int posicao(int numero) {
 		for(int i = 0; i < contas.size(); i++) {
-			if(contas.get(numero).getNumero(i) == numero) {
+			if(contas.get(i).getNumero(0) == numero) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	//retorna -1 se não achar
 	
-	public boolean remover(int codigo) {
-        int posicao = posicao(codigo);
-        if (posicao >= 0) { // se achou
-            if (contas.remove(posicao) != null) {
-                return true;
-            }
-        }
-        return false;
-    }
+	
+	
+	
+	
+	public boolean remover(int numero) {
+		int posicao= posicao(numero);
+		if(posicao >= 0) {
+			if(contas.remove(posicao) != null) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+	
+	
+	//fim da class 
+	//GerenciaContas
+	
 }
